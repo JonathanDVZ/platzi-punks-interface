b88762dd35441985b6a3601e652a19579f32f8c6
JonathanDVZ <jodaviza@gmail.com>
2022-12-13 16:05:08 -0300

    ABI added

--- src/config/web3/index.js
+++ src/config/web3/index.js
@@ -2,7 +2,7 @@ import Web3 from "web3";
 import { InjectedConnector } from "@web3-react/injected-connector";
 
 const injected = new InjectedConnector({
-  supportedChainIds: [1, 3, 4, 5, 42],
+  supportedChainIds: [5],
 });
 
 const getLibrary = (provider) => {
